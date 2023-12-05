73741cbfe5655ea425569fba9070a8152f9246bb
Abbaoui Katia <katia.abbaoui@efrei.net>
2023-12-05 16:51:29 +0100

    Remove unused imports

--- backend/common.py
+++ backend/common.py
@@ -1,27 +1,11 @@
 # Standard library imports
 from datetime import datetime, date
-import json
-import math
 import time
 
 # Related third party imports
-from bokeh.layouts import gridplot
-from bokeh.models import Panel, Tabs
-from bokeh.palettes import Category20c
-from bokeh.plotting import figure
-from bokeh.transform import cumsum
-import altair as alt
-from pandas.io.json import json_normalize
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from PIL import Image
-import plotly.express as px
-import plotly.figure_factory as ff
-import pydeck as pdk
-import seaborn as sns
 import streamlit as st
-from tkinter import filedialog
 
 def get_image(path: str) -> Image:
     """Load an image from the given path.
@@ -51,15 +35,6 @@ def get_week(dt):
     """Extract the week from a datetime object."""
     return dt.week
 
-def by_month(data, year, column):
-    """Aggregate data by month for a specific year."""
-    agg_by_month = data[data["year"] == year].groupby("month")[column].sum()
-    return pd.DataFrame(agg_by_month).reset_index()
-
-def by_week(data, year, column):
-    """Aggregate data by week for a specific year."""
-    agg_by_week = data[data["year"] == year].groupby("week")[column].sum()
-    return pd.DataFrame(agg_by_week).reset_index()
 
 @st.cache()
 def get_lat_long(x, processing_media_list=False):
--- frontend/src/common.py
+++ frontend/src/common.py
@@ -1,28 +1,5 @@
-
-# Standard library imports
-from datetime import datetime, date
-import json
-import math
-import time
-
 # Related third party imports
-from bokeh.layouts import gridplot
-from bokeh.models import Panel, Tabs
-from bokeh.palettes import Category20c
-from bokeh.plotting import figure
-from bokeh.transform import cumsum
-import altair as alt
-from pandas.io.json import json_normalize
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from PIL import Image
-import plotly.express as px
-import plotly.figure_factory as ff
-import pydeck as pdk
-import seaborn as sns
-import streamlit as st
-from tkinter import filedialog
 
 def by_month(data, year, column):
     """Aggregate data by month for a specific year."""
