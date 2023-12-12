cee8016caabfeac0d77aca4eadbdc32840b4adc9
Xiangyu AN <xiangyu.an@efrei.net>
2023-12-12 06:57:18 +0100

    E2e test

--- frontend/src/view/common.py
+++ frontend/src/view/common.py
@@ -7,8 +7,6 @@ import streamlit as st
 from PIL import Image
 
 
-
-@st.cache()
 def separate_percentage(df: pd.DataFrame, column: str):
     """Separate and transform a column into percentage in another DataFrame.
     Args:
@@ -20,19 +18,21 @@ def separate_percentage(df: pd.DataFrame, column: str):
     """
     # wait for the data to be loaded
     time.sleep(1)
-    
+
     df_cp = df.copy()
-    
+
     # transform the column into a list
     ma_list = list(df_cp[column])
     # remove the percentage sign and replace the comma with a dot
-    ma_list = list(map(lambda x: float(x.replace(',', '.').replace('%', '')), ma_list))
+    ma_list = list(
+        map(lambda x: float(x.replace(',', '.').replace('%', '')), ma_list))
     # delete the column
     df_cp.drop([column], axis=1)
     # transform the list of the specific column into a DataFrame with the Valeur as column name
     df_value = pd.DataFrame(ma_list).rename(columns={0: "Valeur"}, index={})
-    
+
     return df_value, df_cp
+
 
 def get_image(path: str) -> Image:
     """Load an image from the given path.
